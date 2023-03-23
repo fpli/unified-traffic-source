62c8d3f93329f80e223647bb9eac8e0be7a18858
cmao <cmao@ebay.com>
2023-03-23 14:13:39 +0800

    update schema: add ubi_w.uts_unified_session_v1_no_trfc_src

--- batch-daily/v2/pipeline/schema/traffic-source-schema.sql
+++ batch-daily/v2/pipeline/schema/traffic-source-schema.sql
@@ -11,11 +11,12 @@
 -- ubi_w.uts_v2_traffic_source_2
 -- ubi_w.uts_v2_unified_traffic_source
 -- ubi_w.uts_v2_unified_traffic_source_backup
+-- ubi_w.uts_unified_session_v1_no_trfc_src
 --
 -- https://zeta.dss.vip.ebay.com/zeta/share/#/notebook?notebookId=8507a3da-982d-423e-a038-10d34f2c8ac4
 --------------------------------------------------------------------------------
 
-drop table if exists ubi_w.uts_v2_unified_session_copy
+drop table if exists ubi_w.uts_v2_unified_session_copy;
 CREATE TABLE ubi_w.uts_v2_unified_session_copy (
   `guid` STRING,
   `global_session_id` STRING,
@@ -35,7 +36,7 @@ OPTIONS (
 PARTITIONED BY (dt, data_type, session_type)
 LOCATION 'viewfs://apollo-rno/sys/edw/working/ubi/ubi_w/soj/uts_v2_unified_session_copy';
 
-drop table if exists ubi_w.uts_v2_surface_event
+drop table if exists ubi_w.uts_v2_surface_event;
 CREATE TABLE ubi_w.uts_v2_surface_event (
   `global_session_id` STRING,
   `source` STRING,
@@ -52,7 +53,7 @@ OPTIONS (
   `compression` 'snappy')
 LOCATION 'viewfs://apollo-rno/sys/edw/working/ubi/ubi_w/soj/uts_v2_surface_event';
 
-drop table if exists ubi_w.uts_v2_ubi_event
+drop table if exists ubi_w.uts_v2_ubi_event;
 CREATE TABLE ubi_w.uts_v2_ubi_event (
   `global_session_id` STRING,
   `source` STRING,
@@ -69,7 +70,7 @@ OPTIONS (
   `compression` 'snappy')
 LOCATION 'viewfs://apollo-rno/sys/edw/working/ubi/ubi_w/soj/uts_v2_ubi_event';
 
-drop table if exists ubi_w.uts_v2_deeplink_event
+drop table if exists ubi_w.uts_v2_deeplink_event;
 CREATE TABLE ubi_w.uts_v2_deeplink_event (
   `global_session_id` STRING,
   `source_session_skey` BIGINT,
@@ -83,7 +84,7 @@ OPTIONS (
   `compression` 'snappy')
 LOCATION 'viewfs://apollo-rno/sys/edw/working/ubi/ubi_w/soj/uts_v2_deeplink_event';
 
-drop table if exists ubi_w.uts_v2_first_valid_event
+drop table if exists ubi_w.uts_v2_first_valid_event;
 CREATE TABLE ubi_w.uts_v2_first_valid_event (
   `guid` STRING,
   `global_session_id` STRING,
@@ -99,7 +100,7 @@ OPTIONS (
   `compression` 'snappy')
 LOCATION 'viewfs://apollo-rno/sys/edw/working/ubi/ubi_w/soj/uts_v2_first_valid_event';
 
-drop table if exists ubi_w.uts_v2_utp_event
+drop table if exists ubi_w.uts_v2_utp_event;
 CREATE TABLE ubi_w.uts_v2_utp_event (
   `global_session_id` STRING,
   `source_session_skey` BIGINT,
@@ -116,7 +117,7 @@ OPTIONS (
   `compression` 'snappy')
 LOCATION 'viewfs://apollo-rno/sys/edw/working/ubi/ubi_w/soj/uts_v2_utp_event';
 
-drop table if exists ubi_w.uts_v2_imbd_event
+drop table if exists ubi_w.uts_v2_imbd_event;
 CREATE TABLE ubi_w.uts_v2_imbd_event (
   global_session_id string,
   source_session_skey bigint,
@@ -130,7 +131,7 @@ OPTIONS (
   `compression` 'snappy')
 LOCATION 'viewfs://apollo-rno/sys/edw/working/ubi/ubi_w/soj/uts_v2_imbd_event';
 
-drop table if exists ubi_w.uts_v2_traffic_source_1
+drop table if exists ubi_w.uts_v2_traffic_source_1;
 CREATE TABLE ubi_w.uts_v2_traffic_source_1 (
   `global_session_id` STRING,
   `guid` STRING,
@@ -148,7 +149,7 @@ OPTIONS (
   `compression` 'snappy')
 LOCATION 'viewfs://apollo-rno/sys/edw/working/ubi/ubi_w/soj/uts_v2_traffic_source_1';
 
-drop table if exists ubi_w.uts_v2_traffic_source_2
+drop table if exists ubi_w.uts_v2_traffic_source_2;
 CREATE TABLE ubi_w.uts_v2_traffic_source_2 (
   `global_session_id` STRING,
   `guid` STRING,
@@ -165,7 +166,7 @@ OPTIONS (
   `compression` 'snappy')
 LOCATION 'viewfs://apollo-rno/sys/edw/working/ubi/ubi_w/soj/uts_v2_traffic_source_2';
 
-drop table if exists ubi_w.uts_v2_unified_traffic_source
+drop table if exists ubi_w.uts_v2_unified_traffic_source;
 CREATE TABLE ubi_w.uts_v2_unified_traffic_source (
   `global_session_id` STRING,
   `guid` STRING,
@@ -182,7 +183,7 @@ OPTIONS (
   `compression` 'snappy')
 LOCATION 'viewfs://apollo-rno/sys/edw/working/ubi/ubi_w/soj/uts_v2_unified_traffic_source';
 
-drop table if exists ubi_w.uts_v2_unified_traffic_source_backup
+drop table if exists ubi_w.uts_v2_unified_traffic_source_backup;
 CREATE TABLE ubi_w.uts_v2_unified_traffic_source_backup (
   `guid` STRING,
   `global_session_id` STRING,
@@ -201,3 +202,23 @@ OPTIONS (
   `compression` 'snappy')
 PARTITIONED BY (dt, data_type, session_type)
 LOCATION 'viewfs://apollo-rno/sys/edw/working/ubi/ubi_w/soj/uts_v2_unified_traffic_source_backup';
+
+drop table if exists `ubi_w`.`uts_unified_session_v1_no_trfc_src`;
+CREATE TABLE `ubi_w`.`uts_unified_session_v1_no_trfc_src` (
+  `guid` STRING,
+  `global_session_id` STRING,
+  `abs_start_timestamp` BIGINT,
+  `abs_end_timestamp` BIGINT,
+  `session_start_dt` BIGINT,
+  `traffic_source` MAP<STRING, STRING>,
+  `bot_flag` STRUCT<`surface`: ARRAY<INT>, `ubi`: ARRAY<INT>, `utp`: ARRAY<INT>>,
+  `others` MAP<STRING, STRING>,
+  `bot_type` STRING,
+  `dt` STRING,
+  `data_type` STRING,
+  `session_type` STRING)
+USING parquet
+OPTIONS (
+  `compression` 'snappy')
+PARTITIONED BY (dt, data_type, session_type)
+LOCATION 'viewfs://apollo-rno/sys/edw/ubi/ubi_t/soj/unified_session_swap/v1_no_trfc_src';

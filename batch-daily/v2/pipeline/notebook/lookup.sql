4c0931d5df9ce770e893b8faa3bd45b03feeb518
cmao <cmao@ebay.com>
2023-08-01 13:28:21 +0800

    add check lookup table count

--- batch-daily/v2/pipeline/notebook/lookup.sql
+++ batch-daily/v2/pipeline/notebook/lookup.sql
@@ -7,6 +7,9 @@ from ACCESS_VIEWS.PAGES
 where page_id is not null and page_name is not null and iframe is not null
 distribute by page_id;
 
+select assert_true(count(1) > 16000)
+from ubi_w.uts_v2_lkp_pages;
+
 insert overwrite ubi_w.uts_v2_lkp_rotations
 select
   rotation_id,
@@ -14,3 +17,6 @@ select
 from CHOCO_DATA_V.DW_MPX_ROTATIONS
 where rotation_id is not null and mpx_chnl_id is not null
 distribute by rotation_id;
+
+select assert_true(count(1) > 400000)
+from ubi_w.uts_v2_lkp_rotations;

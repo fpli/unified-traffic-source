6d81bde4b6ba1e17d47ef7b31fe7328dc05c4b0f
cmao <cmao@ebay.com>
2023-08-01 13:26:23 +0800

    add shell to copy lookup files

---
+++ batch-daily/v2/pipeline/bin/sg_ubi_data.copy_lkp_rotations.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -x
+export HDP_CMD="/apache/hadoop/bin/hadoop"
+
+cnt=$($HDP_CMD fs -find /sys/edw/working/ubi/ubi_w/soj/uts_v2_lkp_rotations -name '*.avro' | wc -l)
+
+if [ $cnt -eq 1 ] ; then
+  echo "There is exactly 1 Avro file. Copy it to target."
+  src=$($HDP_CMD fs -find /sys/edw/working/ubi/ubi_w/soj/uts_v2_lkp_rotations -name '*.avro')
+  dst="/sys/soj/ubd/traffic-source/lookup/lkp_rotations.avro"
+  $HDP_CMD fs -cp -f $src $dst
+  echo "Copied from $src to $dst"
+else
+  echo "Not exactly 1 Avro file. Exit."
+  exit -1
+fi
